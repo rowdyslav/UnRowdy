dc7a5074baa0d0ae820d40d1a098ad7baf23584b
Danila <konaevdanila39@gmail.com>
2025-10-22 18:57:16 +0300

    fix

--- frontend/src/features/service/hooks/useAllServices.ts
+++ frontend/src/features/service/hooks/useAllServices.ts
@@ -3,9 +3,9 @@ import { serviceApi } from '@/shared/api/service.ts'
 import { useQuery } from '@tanstack/react-query'
 import type { FilterQueryTypes } from '@/features/categories/types/Filter.schema.ts'
 
-export const useAllServices = (data: FilterQueryTypes & { category?: string }) => {
+export const useAllServices = (data: FilterQueryTypes & { category_name?: string }) => {
   return useQuery<ServiceApiGetType[]>({
-    queryKey: ['services', 'all', data.category, data.keywords],
+    queryKey: ['services', 'all', data.category_name, data.keywords],
 
     queryFn: async () => {
       const response = await serviceApi.getAllServices({ ...data })
